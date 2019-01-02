09463119cb46da38c5a30646ad68d53cbcb8c14d
Christopher Willis <willis.christopher.a@gmail.com>
2019-01-01 22:54:19 -0600

    Updates

---
+++ precache-manifest.9b37a2d2fb7b137b73243211a0ae74b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "36e01c2fa85c2ffe2810",
+    "url": "/static/js/main.36e01c2f.chunk.js"
+  },
+  {
+    "revision": "f0bcfdb04b8b5d0049cb",
+    "url": "/static/js/1.f0bcfdb0.chunk.js"
+  },
+  {
+    "revision": "36e01c2fa85c2ffe2810",
+    "url": "/static/css/main.0d4f53d3.chunk.css"
+  },
+  {
+    "revision": "590661e731ac9e31365f2d6cc238a4d0",
+    "url": "/index.html"
+  }
+];
