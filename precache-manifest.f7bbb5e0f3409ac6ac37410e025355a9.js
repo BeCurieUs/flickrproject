826cc959b8e23e6201dd7add1a7b297df13b467d
Christopher Willis <willis.christopher.a@gmail.com>
2019-01-02 00:14:24 -0600

    Updates

---
+++ precache-manifest.f7bbb5e0f3409ac6ac37410e025355a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "182f34e76cc4f0a21dbb",
+    "url": "/flickrproject/static/js/runtime~main.182f34e7.js"
+  },
+  {
+    "revision": "8c728aa9d37cd2219d3f",
+    "url": "/flickrproject/static/js/main.8c728aa9.chunk.js"
+  },
+  {
+    "revision": "f0bcfdb04b8b5d0049cb",
+    "url": "/flickrproject/static/js/1.f0bcfdb0.chunk.js"
+  },
+  {
+    "revision": "8c728aa9d37cd2219d3f",
+    "url": "/flickrproject/static/css/main.0d4f53d3.chunk.css"
+  },
+  {
+    "revision": "f2c5d2a2f2030d8e9028436da3b0786c",
+    "url": "/flickrproject/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1868643b049ec60787a0100c34aac45f.js"
+  "/flickrproject/precache-manifest.f7bbb5e0f3409ac6ac37410e025355a9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/flickrproject/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
