6a7139fdf3706ce9e6a3d9dde323c06a25378980
Christopher Willis <willis.christopher.a@gmail.com>
2019-01-02 21:19:52 -0600

    gradable build

--- src/App.js
+++ src/App.js
@@ -8,13 +8,20 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import './App.css';
 const myapi_key = "3347f47687a578f9048de57e5561217f";
+//temperaraly have my own manual API key, will change to individual API key
+// when I learn now to oAuth more
+
 const api_secret = "5c9d856058d4cd88"
+
+// same as above though not needed yet with current feature set
 
 
 class App extends Component {
 
   state = {
     user_id : "11860006@N08",
+    // manually have an ID for now, will be able to input one in time
+    // obtaining your user id is not built into the app yet, has to be manually obtained from flickr
     printCollection : [],
     completePhotostream : [],
   }
@@ -25,16 +32,26 @@ class App extends Component {
 
 
   searchFlickr = () =>{
+    // initial fetch uses the user id to grab all the photo lists (basically their named image gallaries)
+    // This is an array of objects that contain information about the photo gallery (its id) and its default photo
+    // and information about the list itself. This has page limits, will have to build in detection evetually
+    // code now only operates correctly if all the information is on a single page
     fetch(`https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=${myapi_key}&user_id=${this.state.user_id}&format=json&nojsoncallback=1`)
     .then(response => response.json())
     .then(data => {
       // console.log(JSON.stringify(data, null, 2))
       data.photosets.photoset.forEach( (photoCollection,index) => {
+        // each photo gallery also has to be individually looked up for all the photos in that gallery
+        // this is also page limited and only curretly works for lists that have one page of results (500)
         fetch(`https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${myapi_key}&photoset_id=${photoCollection.id}&format=json&nojsoncallback=1`)
         .then(innerResponse => innerResponse.json())
         .then(innerData => {
           // console.log(photoCollection.title._content)
           // console.log(innerData.photoset.photo)
+
+          // complete photostream ends up housing an array of objects of galleries, one of the keys is the title
+          // of the gallery, the other is the data needed to look up a picture link for the default picture
+          // and the final key is an array of all of the pictures data for link lookup
           this.setState({
             completePhotostream : [...this.state.completePhotostream,{title : photoCollection.title._content, farm:photoCollection.farm, id:photoCollection.primary, server:photoCollection.server, secret:photoCollection.secret , photos : innerData.photoset.photo}]
           })
@@ -44,6 +61,8 @@ class App extends Component {
       // this.printImageList(this.state.collectionsList)
     })
   }
+
+  // might be obsolute, keeping around to copy past from
 
   printImageList = (flickrImageArray) =>{
     flickrImageArray.forEach((imageObject,index)=>{
@@ -76,12 +95,18 @@ class App extends Component {
   //   event.preventDefault();
   //   this.setState({searchTerm :event.target.value}) 
   // }
+
+  // link truncation needed for picture link look up
   getSize = () =>{
     if(this.props.size!==undefined){
       return "_"+this.props.size
     }
     return "";
   }
+
+  // had this as componet but ended up making it overly complicated.
+  // flickr has a special way to look up links that involve all the data stored in the complete photo
+  // stream array
 
   urlGenerator = (farm,server,id,secret,size) => {
     return `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}${this.getSize()}.jpg`
@@ -121,10 +146,6 @@ class App extends Component {
                   photoAlbumb.id, photoAlbumb.secret,"q")}
                 title={photoAlbumb.title}
               />
-              {/* <Flickrimg farm={photoAlbumb.farm} server={photoAlbumb.server}
-                id={photoAlbumb.id} secret={photoAlbumb.secret} title={photoAlbumb.title}
-                size="q">
-              </Flickrimg> */}
               <CardContent className="title-card">
                 <Typography variant="title" >{photoAlbumb.title}</Typography>
               </CardContent>
@@ -136,5 +157,13 @@ class App extends Component {
     );
   }
 }
+
+// major functionality still missing. I want to be able to backup sometimes entire photo stream
+// this is complicated because the current method I used to access images doesn't give me access
+// to the orignials, I have to use a different API call now that I have the image IDs.
+
+// I also haven't given a way to view a gallery, sort the galleries (flickr just blasts them out in any order)
+// or to view individual images in a gallery. I want this ability so that people can select or deselect
+// pictures to backup. 
 
 export default App;
